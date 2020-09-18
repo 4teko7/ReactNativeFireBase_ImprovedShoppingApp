54604db7e4ac08a01d6363cd069ab6f226a7d4cd
4teko7 <bilal.tekin@boun.edu.tr>
2020-09-18 19:59:12 +0300

    redundantCodesWereRemovedAndCardComponentWasCreated

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {View,Text,Image,StyleSheet,Button,TouchableOpacity,TouchableNativeFeedback, Platform} from 'react-native';
 import Colors from '../../constants/Colors';
 
+import Card from '../UI/Card';
+
 const ProductItem = props => {
 
     let TouchableCmp = TouchableOpacity;
@@ -11,7 +13,7 @@ const ProductItem = props => {
     }
 
     return (
-        <View style = {styles.product}>
+        <Card style = {styles.product}>
             <View style={styles.touchable}>
                 <TouchableCmp useForeground onPress={props.onViewDetail}>
                     <View>
@@ -28,20 +30,13 @@ const ProductItem = props => {
                     </View>
                 </TouchableCmp>
             </View>
-        </View>
+        </Card>
     );
 }
 
 
 const styles = StyleSheet.create({
     product: {
-        shadowColor: 'black',
-        shadowOpacity: 0.26,
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius:8,
-        elevation:5,
-        borderRadius:10,
-        backgroundColor:'white',
         height: 300,
         margin:20,
     },
--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -5,6 +5,7 @@ import Colors from "../../constants/Colors";
 import CartItem from "../../components/shop/CartItem";
 import * as cartActions from "../../store/actions/cart";
 import * as orderActions from '../../store/actions/orders';
+import Card from '../../components/UI/Card';
 
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
@@ -31,7 +32,7 @@ const CartScreen = (props) => {
   });
   return (
     <View style={styles.screen}>
-      <View style={styles.summary}>
+      <Card style={styles.summary}>
         <Text style={styles.summaryText}>
           Total:
           <Text style={styles.amount}>
@@ -47,7 +48,7 @@ const CartScreen = (props) => {
           }
           }
         />
-      </View>
+      </Card>
       <FlatList
         data={cartItems}
         keyExtractor={(item) => item.productId}
@@ -82,14 +83,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     marginBottom: 20,
-    padding: 10,
-    shadowColor: "black",
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: "white",
+    padding: 10
   },
   summaryText: {
     fontFamily: "Courgette",
